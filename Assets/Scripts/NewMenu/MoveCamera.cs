802b63c0cc7ca0c70bc7039b5449f8fe07216c66
baturayp <baturay@outlook.fr>
2020-03-04 18:18:19 +0300

    Big changes to menu

---
+++ Assets/Scripts/NewMenu/MoveCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public GameObject camCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (SwipeInput.swipedLeft)
+        {
+            camCam.transform.Translate(0,0,1, Space.World);
+        }
+        if (SwipeInput.swipedRight)
+        {
+            camCam.transform.Translate(0, 0, -1, Space.World);
+        }
+        if (SwipeInput.swipedUp)
+        {
+            camCam.transform.Translate(Vector3.up, Space.World);
+        }
+        if (SwipeInput.swipedDown)
+        {
+            camCam.transform.Translate(Vector3.down, Space.World);
+        }
+    }
+}
