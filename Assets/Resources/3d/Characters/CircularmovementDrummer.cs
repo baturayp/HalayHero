8db8752a7dd70ce5c49b2af1f5b5047c9a9b3d52
hursittola <61533529+hursittola@users.noreply.github.com>
2020-03-18 14:07:26 +0300

    Forest and New Drummer

--- Assets/Resources/3d/Characters/CircularmovementDrummer.cs
+++ Assets/Resources/3d/Characters/CircularmovementDrummer.cs
@@ -2,12 +2,12 @@
 
 public class CircularmovementDrummer : MonoBehaviour
 {
-    public float turnMultiplier = 1.6f;
-    public float movementMultiplier = 0;
+    public float turnMultiplier = 2.4f;
+    public float movementMultiplier = 0.0405f;
     public float rotationCounter = 0;
     public float movementCounter = 0;
-    public float radius = 0;
-    float rotationOffset = 0;
+    public float radius = 3.2f;
+    float rotationOffset = -2.2f;
     public float movementOffset;
     Vector3 rotVect;
     Vector3 movementVect;
@@ -15,7 +15,7 @@ public class CircularmovementDrummer : MonoBehaviour
 
     void Start()
     {
-        rotationOffset = this.transform.rotation.eulerAngles.y;;
+        rotationOffset = this.transform.rotation.eulerAngles.y;
     }
 
     void Update()
---
+++ Assets/Resources/3d/Characters/DrumMotionStopper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrumMotionStopper : MonoBehaviour
+{
+
+    public CircularmovementDrummer script;
+
+    void DrummerStop()
+    {
+        script.enabled = false;
+    }
+
+    void DrummerContinue()
+    {
+        script.enabled = true;
+    }
+}
